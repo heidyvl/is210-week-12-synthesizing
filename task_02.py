c9c900a69bdadc19442f23cd82c595e56ad168cc
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-04-22 21:42:01 +0000

    task_02

--- task_02.py
+++ task_02.py
@@ -7,7 +7,6 @@ class CustomError(Exception):
     """Custom Error Class"""
 
     def __init__(self, message, cause):
+        super(CustomError, self).__init__(message)
         self.message = message
         self.cause = cause
-
-
