23607c82a1d90bdd572a2ec760ba3b5a21607d78
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-04-22 21:56:07 +0000

    task_03

--- task_03.py
+++ task_03.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 """Task 03 module"""
 
+
 def exception_test(arg1, arg2, arg3):
+    """Exception test function"""
     caught = False
     try:
         arg1[arg2].index(arg3)
-    except:
+    except (TypeError, LookupError):
         caught = True
-
     return caught
-    
